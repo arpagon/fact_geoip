2388e3a9539eed5f42b274cbe99388dcf38ac966
Sebastian Rojo Suarez <sebastian.rojo@sapian.com.co>
2018-06-19 14:34:29 -0500

    ADDED timeout to geoip_country

--- lib/facter/geoip_country.rb
+++ lib/facter/geoip_country.rb
@@ -1,13 +1,47 @@
-
 require 'facter'
 require 'net/http'
+require 'uri'
 require 'json'
 
-response = Net::HTTP.get_response("api.ipify.org","/?format=json")
-ipaddress = JSON.parse(response.body)['ip']
-response2 = Net::HTTP.get_response("freegeoip.net","/json/" + ipaddress)
-# response2 = Net::HTTP.get_response("freegeoip.net","/json/8.8.8.8")
-geoip = JSON.parse(response2.body)
+begin
+  url = URI.parse("https://api.ipify.org/?format=json")
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.read_timeout = 10
+  http.open_timeout = 10
+  response = http.start() {|http|
+    http.get("/?format=json", {'Content-Type' => 'application/json'})
+  }
+  # response = Net::HTTP.get_response("api.ipify.org","/?format=json")
+  begin
+    ipaddress = JSON.parse(response.body)['ip']
+  rescue JSON::ParserError => e
+    ipaddress = "X.X.X.X"
+  end
+rescue SystemCallError
+  ipaddress = "X.X.X.X"
+end
+
+begin
+  url = URI.parse("https://freegeoip.net/json/" + ipaddress)
+  http = Net::HTTP.new(url.host, url.port)
+
+  http.use_ssl = true
+  http.read_timeout = 10
+  http.open_timeout = 10
+  response2 = http.start() {|http|
+    http.get("/json/" + ipaddress , {'Content-Type' => 'application/json'})
+  }
+  # response2 = Net::HTTP.get_response("freegeoip.net","/json/" + ipaddress)
+  # response2 = Net::HTTP.get_response("freegeoip.net","/json/8.8.8.8")
+  begin
+    geoip = JSON.parse(response2.body)
+  rescue JSON::ParserError => e
+    geoip = {"error" => "No internet connectios"}
+  end
+rescue SystemCallError
+  geoip = {"error"=> "No internet connectios"}
+end
 
 Facter.add('geoip_ipaddress') do
   setcode do
