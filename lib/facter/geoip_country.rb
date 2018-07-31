2bc03fcb57556cb902a57ae1b39a0e6cdd1760e5
Sebastian Rojo Suarez <sebastian.rojo@sapian.com.co>
2018-07-31 14:36:47 -0500

    geoip Sapian Services

--- lib/facter/geoip_country.rb
+++ lib/facter/geoip_country.rb
@@ -4,6 +4,31 @@ require 'uri'
 require 'json'
 
 begin
+  url = URI.parse("https://ipify.dialbox.cloud/?format=json")
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.read_timeout = 10
+  http.open_timeout = 10
+  response = http.start() {|http|
+    http.get("/?format=json", {'Content-Type' => 'application/json'})
+  }
+  # response = Net::HTTP.get_response("api.ipify.org","/?format=json")
+  begin
+    ipify_sapian_ipaddress = JSON.parse(response.body)['ip']
+    get_repo_sapian = true
+  rescue JSON::ParserError => e
+    ipify_sapian_ipaddress = "X.X.X.X"
+    get_repo_sapian = false
+  end
+rescue SystemCallError
+  ipify_sapian_ipaddress = "X.X.X.X"
+  get_repo_sapian = false
+rescue
+  ipify_sapian_ipaddress = "X.X.X.X"
+  get_repo_sapian = false
+end
+
+begin
   url = URI.parse("https://api.ipify.org/?format=json")
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
@@ -15,11 +40,17 @@ begin
   # response = Net::HTTP.get_response("api.ipify.org","/?format=json")
   begin
     ipaddress = JSON.parse(response.body)['ip']
+    open_internet = true
   rescue JSON::ParserError => e
     ipaddress = "X.X.X.X"
+    open_internet = false
   end
 rescue SystemCallError
   ipaddress = "X.X.X.X"
+  open_internet = false
+rescue
+  ipaddress = "X.X.X.X"
+  open_internet = false
 end
 
 begin
@@ -39,13 +70,32 @@ begin
   rescue JSON::ParserError => e
     geoip = {"error" => "No internet connectios"}
   end
-rescue SystemCallError
+rescue
   geoip = {"error"=> "No internet connectios"}
 end
+
+Facter.add('geoip_ipify_sapian_ipaddress') do
+  setcode do
+    ipify_sapian_ipaddress
+  end
+end
+
+Facter.add('geoip_get_repo_sapian') do
+  setcode do
+    get_repo_sapian
+  end
+end
+
 
 Facter.add('geoip_ipaddress') do
   setcode do
     ipaddress
+  end
+end
+
+Facter.add('geoip_open_internet') do
+  setcode do
+    open_internet
   end
 end
 
